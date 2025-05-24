ddf8173ab838d6f66cb42d79704b9c32b3413771
ryardley <contact@rudiyardley.com>
2025-05-24 23:59:30 +0200

    Udate to use avante

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,28 @@
+return {
+	"yetone/avante.nvim",
+	event = "VeryLazy",
+	version = false, -- Never set this value to "*"! Never!
+	opts = {
+		-- add any opts here
+		-- for example
+		provider = "claude",
+		claude = {
+			endpoint = "https://api.anthropic.com",
+			model = "claude-opus-4-20250514", -- your desired model (or use gpt-4o, etc.)
+			timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+			temperature = 0,
+			max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+			disabled_tools = { "python" },
+			--reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+		},
+	},
+	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+	build = "make",
+	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"stevearc/dressing.nvim",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+	},
+}
--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,5 +1,5 @@
 return {
   "m4xshen/hardtime.nvim",
   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  opts = { enabled = false, disable_mouse = false, max_count = 5 },
+  opts = { enabled = false, disable_mouse = false, max_count = 15 },
 }
