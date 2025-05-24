825c3d49a411bab2787049407e46a2c23dbf74ce
ryardley <contact@rudiyardley.com>
2025-05-25 01:19:56 +0200

    Update

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,19 +1,23 @@
 return {
 	"yetone/avante.nvim",
 	event = "VeryLazy",
-	version = false, -- Never set this value to "*"! Never!
+	version = "v0.0.23", -- Never set this value to "*"! Never!
 	opts = {
-		-- add any opts here
-		-- for example
 		provider = "claude",
+		auto_suggestions_provider = "claude",
 		claude = {
 			endpoint = "https://api.anthropic.com",
-			model = "claude-opus-4-20250514", -- your desired model (or use gpt-4o, etc.)
-			timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+			model = "claude-3-5-sonnet-20241022",
+			timeout = 30000, -- Timeout in milliseconds
 			temperature = 0,
-			max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-			disabled_tools = { "python" },
-			--reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+			max_tokens = 8192,
+			disable_tools = true, -- disable tools!
+		},
+		behaviour = {
+			auto_suggestions = false,
+			minimize_diff = false,
+			enable_cursor_planning_mode = true,
+			enable_claude_text_editor_tool_mode = true,
 		},
 	},
 	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
