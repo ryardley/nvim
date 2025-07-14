c9a93f42b75d8622c48bfde672cfbc8ae4293bf5
ryardley <contact@rudiyardley.com>
2025-07-14 15:59:51 +0400

    Update oil config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -24,6 +24,19 @@ return {
         ["<M-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
       },
     })
+
+    -- Create autocmd to override Oil's default navigation keys
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "oil",
+      callback = function()
+        local nvim_tmux_nav = require("nvim-tmux-navigation")
+        vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft, { buffer = true })
+        vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown, { buffer = true })
+        vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp, { buffer = true })
+        vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight, { buffer = true })
+      end,
+    })
+
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory", noremap = true, silent = true })
   end,
 }
