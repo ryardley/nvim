030150a575327538d7f0ad5501b27922f1dd3e46
ryardley <r@blockhackers.io>
2025-09-16 21:12:33 +0200

    update keybinding for sc

--- lua/plugins/stay-centered.lua
+++ lua/plugins/stay-centered.lua
@@ -1,3 +1,22 @@
 return {
-  "arnamak/stay-centered.nvim"
+  "arnamak/stay-centered.nvim",
+  lazy = false,
+  name = "stay-centered",
+  priority = 1000,
+  keys = {
+    {
+      "<leader>st",
+      function()
+        require("stay-centered").toggle()
+      end,
+      desc = "Toggle stay-centered.nvim",
+    },
+  },
+  opts = {
+    enabled = true,
+    skip_filetypes = { "lua", "typescript" },
+  },
+  config = function(_, opts)
+    require("stay-centered").setup(opts)
+  end,
 }
