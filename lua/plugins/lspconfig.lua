066a5139b5a94f77640ede71ebbd150612cb9161
ryardley <r@blockhackers.io>
2026-01-27 16:42:03 +1100

    add space q to show all errors in the lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -127,7 +127,9 @@ return {
     -- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
     vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
     vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-    -- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+    vim.keymap.set('n', '<space>q', function()
+      vim.diagnostic.setqflist({ severity = vim.diagnostic.severity.ERROR })
+    end, { desc = "Errors to quickfix" })
 
     vim.api.nvim_create_autocmd("LspAttach", {
       group = vim.api.nvim_create_augroup("UserLspConfig", {}),
