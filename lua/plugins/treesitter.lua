eba80be6b3563b64d172d66698df5c6f47993080
ryardley <r@blockhackers.io>
2026-01-27 23:57:48 +0000

    update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,19 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   build = ":TSUpdate",
   dependencies = {
     "JoosepAlviste/nvim-ts-context-commentstring",
   },
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "yaml" },
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
+    require("nvim-treesitter").install({ "yaml" })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "yaml" },
+      callback = function()
+        vim.treesitter.start()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
   end,
 }
