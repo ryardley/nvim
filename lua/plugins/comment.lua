313f921b27bb35ed5d414c28d7602e9ff06f419d
ryardley <r@blockhackers.io>
2026-04-14 05:43:24 +0000

    use folke comments

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,14 +1,10 @@
 return {
-  "numToStr/Comment.nvim",
-  dependencies = "JoosepAlviste/nvim-ts-context-commentstring",
-  config = function()
-    require("Comment").setup({
-      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-    })
-
-    vim.keymap.set("n", "<leader>/", function()
-      require("Comment.api").toggle.linewise.current()
-    end, { noremap = true, silent = true })
-    vim.keymap.set("v", "<leader>/", "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>")
-  end,
+  "folke/ts-comments.nvim",
+  opts = {},
+  event = "VeryLazy",
+  enabled = vim.fn.has("nvim-0.10.0") == 1,
+  keys = {
+    { "<leader>/", "gcc", mode = "n", remap = true, silent = true, desc = "Toggle comment" },
+    { "<leader>/", "gc",  mode = "v", remap = true, silent = true, desc = "Toggle comment" },
+  },
 }
