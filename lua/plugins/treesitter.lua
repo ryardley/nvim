fc8cfc6a9f61c6144963874d42d4995ff89a16ef
ryardley <r@blockhackers.io>
2026-05-01 09:34:59 +0700

    use auto

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,10 +9,6 @@ return {
   config = function()
     require("nvim-treesitter").install({ "yaml" })
 
-    require("ts_context_commentstring").setup({
-      enable_autocmd = false,
-    })
-
     vim.api.nvim_create_autocmd("FileType", {
       pattern = { "yaml" },
       callback = function()
