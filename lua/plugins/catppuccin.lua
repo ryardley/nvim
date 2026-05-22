3b3154cb68d4e8341a48cc231abc6d22826df3c8
ryardley <r@blockhackers.io>
2026-05-22 13:11:14 +0700

    tidy up theme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,6 +4,7 @@ return {
     lazy = false,
     enabled = false,
     name = "catppuccin",
+    enabled = false,
     priority = 1000,
     config = function()
       vim.cmd.colorscheme("catppuccin-mocha")
--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,13 +1,15 @@
 return {
   "ellisonleao/gruvbox.nvim",
-  priority = 1000, -- load before other plugins so colors apply correctly
-  enabled = false,
+  priority = 1000,
   config = function()
     require("gruvbox").setup({
-      contrast = "", -- "hard", "soft", or "" for medium
+      contrast = "hard", -- "hard", "soft", or ""
       transparent_mode = false,
     })
     vim.o.background = "dark" -- or "light"
     vim.cmd.colorscheme("gruvbox")
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
   end,
 }
